b2530ea87d8b77d102636f6948a80b7578c67bd2
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-10-05 16:37:54 +0000

    lint fix

--- server.ts
+++ server.ts
@@ -6,15 +6,15 @@ const server: Server = new Server(app);
 const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8080;
 
 app.get("", (_req, res) => {
-	res.send("Hello world!");
+  res.send("Hello world!");
 });
 
 app.listen(PORT, () => {
-	console.log(`Server listen on http://localhost:${PORT}`)
+  console.log(`Server listen on http://localhost:${PORT}`)
 }).on("error", (err: any) => {
-	if (err.code === "EADDRINUSE") {
-		console.error("Error: address address aleardy in use");
-	} else {
-		console.log(err);		
-	}
+  if (err.code === "EADDRINUSE") {
+    console.error("Error: address address aleardy in use");
+  } else {
+    console.log(err);		
+  }
 });
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,17 @@ import express, { Application } from 'express';
 import cors, { CorsOptions } from 'cors';
 
 export default class Server {
-	constructor(app: Application) {
-		this.config(app);		
-	}
+  constructor(app: Application) {
+    this.config(app);		
+  }
 
-	private config(app: Application): void {
-		const corsOptions: CorsOptions = {
-			origin: "http://localhost",			
-		}
+  private config(app: Application): void {
+    const corsOptions: CorsOptions = {
+      origin: "http://localhost",			
+    }
 
-		app.use(cors(corsOptions));
-		app.use(express.json());
-		app.use(express.urlencoded({ extended: true }));
-	}
+    app.use(cors(corsOptions));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+  }
 }
