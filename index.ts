bc7a83f5c6772286a85bbcb52d3e155b1348997a
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-10-15 20:40:00 +0000

    core(): return json response

--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
-import express from 'express';
-import { env } from 'node:process';
+import express, { Request, Response } from "express";
+import { env } from "node:process";
 
 const app = express();
 const PORT = env.PORT;
 
-app.get('/', (_req, res) => {
-    res.send('Ready. Set. Go!!!');
-})
+app.get("/", (_req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({'message': "Ready. Set. Go!!!"});
+});
 
 app.listen(PORT, () => {
-    console.log(`server is listen on port ${PORT}`)
-})
+  console.log(`server is listen on port ${PORT}`);
+});
