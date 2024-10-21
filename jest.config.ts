3d45c3dad2ffa33fba2e3d3dcece3f94b41026be
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-10-21 14:52:30 +0000

    test(config jest): update config jest

--- jest.config.ts
+++ jest.config.ts
@@ -67,7 +67,11 @@ const config: Config = {
     // globalTeardown: undefined,
 
     // A set of global variables that need to be available in all test environments
-    // globals: {},
+    globals: {
+        'ts-test': {
+            useESM: true,
+        }
+    },
 
     // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
     // maxWorkers: "50%",
@@ -123,12 +127,12 @@ const config: Config = {
     // restoreMocks: false,
 
     // The root directory that Jest should scan for tests and modules within
-    // rootDir: undefined,
+    rootDir: ".",
 
     // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //   "<rootDir>"
-    // ],
+    roots: [
+        "<rootDir>/tests"
+    ],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
