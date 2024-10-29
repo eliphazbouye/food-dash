c40a0c2396482c10c2e2f136a3160380fd86fa21
Eliphaz Bouye <eliphazbouye@gmail.com>
2024-10-29 01:26:35 +0000

    core(): use auth services for signup

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,39 +3,56 @@ import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt';
 import { HttpException } from "../../common/exceptions";
 import { HttpStatus } from "../../common/enums";
+import { Prisma } from "@prisma/client";
 
 
 const signIn = async (email: string, plainTextpassword: string) => {
-    try {
-        const customer = await prisma.customer.findUniqueOrThrow({
-            where: { email: email },
-        });
+    const customer = await prisma.customer.findUniqueOrThrow({
+        where: { email: email },
+    });
 
-        const { password, ...customerInfo } = customer;
-        const isMatch = await bcrypt.compare(plainTextpassword, password);
+    const { password, ...customerInfo } = customer;
+    const isMatch = await bcrypt.compare(plainTextpassword, password);
 
-        if (!isMatch) {
-            throw new Error('Password is incorrect!');
-        }
-
-        const payload = {
-            sub: customerInfo.id,
-            email: customerInfo.email,
-        };
-
-        const accessToken = jwt.sign(payload, String(process.env.JWT_SECRET), {
-            expiresIn: "1h",
-        });
-
-        await prisma.customer.update({
-            where: { id: payload.sub },
-            data: { lastLogin: new Date().toISOString() },
-        });
-
-        return { access_token: accessToken };
-    } catch (err: any) {
-        throw new HttpException(err.message, HttpStatus.NOT_FOUND)
+    if (!isMatch) {
+        throw new HttpException(`Password is incorrect!`, HttpStatus.BAD_REQUEST);
     }
+
+    const payload = {
+        sub: customerInfo.id,
+        email: customerInfo.email,
+    };
+
+    const accessToken = jwt.sign(payload, String(process.env.JWT_SECRET), {
+        expiresIn: "1h",
+    });
+
+    await prisma.customer.update({
+        where: { id: payload.sub },
+        data: { lastLogin: new Date().toISOString() },
+    });
+
+    return { access_token: accessToken };
 }
 
-export const authService = { signIn }
+const signUp = async (data: Prisma.CustomerCreateInput) => {
+    const customerAlreadyExist = await prisma.customer.findUnique({
+        where: { email: data.email },
+    });
+
+    if (customerAlreadyExist) {
+        throw new HttpException(`This email has already connected to a account please Sign In!`,
+            HttpStatus.BAD_REQUEST);
+    }
+
+    const passwordHashed = await bcrypt.hash(data.password, 10);
+
+    await prisma.customer.create({ data: {
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: passwordHashed
+    } });
+}
+
+export const authService = { signIn, signUp }
